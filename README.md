c74e7f22769dfefc9f132548bbdbf3a54187f5c1
Ignacio R. de Almeida <ignacio.rdea@gmail.com>
2021-06-09 12:19:29 -0600

    Readme updated

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # DatalogInterpreterMain
- Datalog Interpreter comprised of three components: the Lexer, the Parser, and the Execution engine
+ Datalog Interpreter compromised of three components: the Lexer, the Parser, and the Execution engine
+
+A lexer is a program that takes as input a program file and scans the file for tokens. The
+output to the lexer is a collection of tokens. These tokens can then be read by the parser.
+The parser determines the meaning of the program based on the order and type of tokens.
+This meaning is then given to the execution engine, which executes the program.
+
+A parser is a program that takes as input a collection of tokens (from a lexer). The parser
+then tries to parse the tokens according to the programming language grammar to determine
+correctness. The parser also determines the meaning of the program based on the order and
+type of tokens. This meaning is then given to the execution engine, which executes the
+program.
+
+An execution engine is a program that takes as input the “meaning” of source code (from a
+parser). It executes the meaning of the program defined by the source code. For this project,
+you will implement the beginning of a Datalog execution engine, the last piece you need for
+the Datalog interpreter.
+
+See e https://www.geeksforgeeks.org/compiler-vs-interpreter-2/ for
+details about interpreters.
--- cmake-build-debug/CMakeFiles/project-1.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/project-1.dir/DependInfo.cmake
@@ -4,22 +4,28 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/users/guest/i/ignacioh/CS-236/Project-2/ColonAutomaton.cpp" "/users/guest/i/ignacioh/CS-236/Project-2/cmake-build-debug/CMakeFiles/project-1.dir/ColonAutomaton.cpp.o"
-  "/users/guest/i/ignacioh/CS-236/Project-2/ColonDashAutomaton.cpp" "/users/guest/i/ignacioh/CS-236/Project-2/cmake-build-debug/CMakeFiles/project-1.dir/ColonDashAutomaton.cpp.o"
-  "/users/guest/i/ignacioh/CS-236/Project-2/CommentAutomata.cpp" "/users/guest/i/ignacioh/CS-236/Project-2/cmake-build-debug/CMakeFiles/project-1.dir/CommentAutomata.cpp.o"
-  "/users/guest/i/ignacioh/CS-236/Project-2/DatalogProgram.cpp" "/users/guest/i/ignacioh/CS-236/Project-2/cmake-build-debug/CMakeFiles/project-1.dir/DatalogProgram.cpp.o"
-  "/users/guest/i/ignacioh/CS-236/Project-2/IDAutomata.cpp" "/users/guest/i/ignacioh/CS-236/Project-2/cmake-build-debug/CMakeFiles/project-1.dir/IDAutomata.cpp.o"
-  "/users/guest/i/ignacioh/CS-236/Project-2/Lexer.cpp" "/users/guest/i/ignacioh/CS-236/Project-2/cmake-build-debug/CMakeFiles/project-1.dir/Lexer.cpp.o"
-  "/users/guest/i/ignacioh/CS-236/Project-2/MatcherAutomaton.cpp" "/users/guest/i/ignacioh/CS-236/Project-2/cmake-build-debug/CMakeFiles/project-1.dir/MatcherAutomaton.cpp.o"
-  "/users/guest/i/ignacioh/CS-236/Project-2/Parameter.cpp" "/users/guest/i/ignacioh/CS-236/Project-2/cmake-build-debug/CMakeFiles/project-1.dir/Parameter.cpp.o"
-  "/users/guest/i/ignacioh/CS-236/Project-2/Parser.cpp" "/users/guest/i/ignacioh/CS-236/Project-2/cmake-build-debug/CMakeFiles/project-1.dir/Parser.cpp.o"
-  "/users/guest/i/ignacioh/CS-236/Project-2/Predicate.cpp" "/users/guest/i/ignacioh/CS-236/Project-2/cmake-build-debug/CMakeFiles/project-1.dir/Predicate.cpp.o"
-  "/users/guest/i/ignacioh/CS-236/Project-2/Rule.cpp" "/users/guest/i/ignacioh/CS-236/Project-2/cmake-build-debug/CMakeFiles/project-1.dir/Rule.cpp.o"
-  "/users/guest/i/ignacioh/CS-236/Project-2/StringAutomata.cpp" "/users/guest/i/ignacioh/CS-236/Project-2/cmake-build-debug/CMakeFiles/project-1.dir/StringAutomata.cpp.o"
-  "/users/guest/i/ignacioh/CS-236/Project-2/Token.cpp" "/users/guest/i/ignacioh/CS-236/Project-2/cmake-build-debug/CMakeFiles/project-1.dir/Token.cpp.o"
-  "/users/guest/i/ignacioh/CS-236/Project-2/main.cpp" "/users/guest/i/ignacioh/CS-236/Project-2/cmake-build-debug/CMakeFiles/project-1.dir/main.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/ColonAutomaton.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/ColonAutomaton.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/ColonDashAutomaton.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/ColonDashAutomaton.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/CommentAutomata.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/CommentAutomata.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/Database.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/Database.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/DatalogProgram.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/DatalogProgram.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/Graph.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/Graph.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/Header.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/Header.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/IDAutomata.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/IDAutomata.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/Interpreter.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/Interpreter.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/Lexer.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/Lexer.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/MatcherAutomaton.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/MatcherAutomaton.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/Parameter.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/Parameter.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/Parser.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/Parser.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/Predicate.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/Predicate.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/Relation.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/Relation.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/Rule.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/Rule.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/StringAutomata.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/StringAutomata.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/Token.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/Token.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/Tuple.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/Tuple.cpp.o"
+  "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/main.cpp" "/Users/nacho/OneDrive/BYU/CS-236/DatalogInterpreter/cmake-build-debug/CMakeFiles/project-1.dir/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
